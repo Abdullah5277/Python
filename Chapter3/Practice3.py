91b2893114f5b0f31596c32e795b37457dcbaa80
Abdullah5277 <abdullahramzaan02@gmail.com>
2026-05-20 01:39:42 -0500

    Add files via upload

---
+++ Chapter3/Practice3.py
@@ -0,0 +1,5 @@
+name =input("Enter your name ")
+print(f"Good afternoon ,{name}")
+
+a='''name  you are selcted and date? '''
+print(a.replace("name ","Abdullah").replace("date?","23 Feb 2027"))
---
+++ Chapter3/Strings.py
@@ -0,0 +1,5 @@
+name ="Abdullah "
+name_sort=name[0:4]
+print(name_sort)   #Start from 0 index and write up to 4 index nut 4 index are excluded only and it print   
+b="dullah is bad \"boy\" or nice guy" 
+print(b.replace ("dullah","HAji"))
