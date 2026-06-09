d57dab09741076bc53482aa99b1e5c6b5d389a06
Abdullah5277 <abdullahramzaan02@gmail.com>
2026-06-08 19:08:46 -0500

    Add files via upload

---
+++ Chapter13/lambda.py
@@ -0,0 +1,25 @@
+from functools import reduce
+from flask import flask 
+# # square=lambda x:x*x
+
+# # print(square(5))
+
+# # sum=lambda x,y,z:x+y+z
+
+# # print(sum(1,2,3))
+
+
+# # #jain methods it works for string
+
+# # a=["Ghora","Yammaa","SP","Ebaby"]
+# # result=" :: ".join(a)
+# # print(result) 
+
+# l=[1,2,4,5,56,7]
+# def sum (a,b):
+#     return a+b
+# # def even(n):
+# #     if(n%2==0):
+# #         return True
+# #     return False
+# print(reduce(sum,l))
